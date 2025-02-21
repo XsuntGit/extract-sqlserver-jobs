87bebb696c8c0f2ff22b27d4bce3fe12eaba4c8a
svc-xsunt-jenkins <svc-jenkins@xsunt.com>
2025-02-20 22:25:42 -0500

    Push latest 2025-02-20 10:25:42

--- 172.25.99.101/1APS Oncology Weekly Run.sql
+++ 172.25.99.101/1APS Oncology Weekly Run.sql
@@ -54,7 +54,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'every sat
 		@freq_recurrence_factor=1, 
 		@active_start_date=20210129, 
 		@active_end_date=99991231, 
-		@active_start_time=140001, 
+		@active_start_time=160001, 
 		@active_end_time=235959, 
 		@schedule_uid=N'dbbab674-ca2c-4366-a406-05ef3e55130e'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
