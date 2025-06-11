69aafeee230f47984344a240562965d74935ad07
svc-xsunt-jenkins <svc-jenkins@xsunt.com>
2025-06-10 22:27:52 -0400

    Push latest 2025-06-10 10:27:52

---
+++ 172.25.99.103/1APS DB Role Assigment OnelookDBUser.sql
@@ -0,0 +1,193 @@
+﻿BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'1APS_DB Role Assigment_OnelookDBUser', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'XSUNT\prateek.singh', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Assign OneLookDbUSer to Dbs', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'Use [Master]
+ go
+
+Declare @V_Sql nvarchar (4000) , @dbname nvarchar(500) 
+
+DECLARE Db_cursor CURSOR FOR
+SELECT
+       distinct Name 
+FROM
+            [Master].sys.databases
+ where create_date >= cast(getdate() -1  as date)
+ and name not in (
+''master''
+,''model''
+,''msdb''
+,''tempdb''
+)
+and not  (name like ''%BMSONC%''
+or name like ''%onelook%''
+or name like ''%HIT%''
+or name like ''%account%''
+)
+and name not  like ''%BMS%CHIN%''
+and name not like ''%XsuntAdmin%''
+and name not like ''%BMSHEME%''
+OPEN Db_cursor
+FETCH NEXT
+FROM
+      Db_cursor
+INTO
+      @dbname
+ 
+WHILE @@FETCH_STATUS = 0
+Begin
+print @dbname
+		set @V_SQL =N''	use '' + @dbName + ''
+		                     
+				 
+							 ALTER AUTHORIZATION ON SCHEMA::db_ddladmin TO dbo;
+							 ALTER AUTHORIZATION ON SCHEMA::db_datawriter TO dbo;
+							 ALTER AUTHORIZATION ON SCHEMA::db_datareader TO dbo;
+
+							IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N''''OnelookDBUser'''')
+								DROP USER OnelookDBUser
+ 
+
+							CREATE USER [OnelookDBUser] FOR LOGIN [OnelookDBUser]
+
+							ALTER USER [OnelookDBUser] WITH DEFAULT_SCHEMA=[dbo]
+
+							ALTER ROLE [db_datareader] ADD MEMBER [OnelookDBUser]
+							ALTER ROLE [db_datawriter] ADD MEMBER [OnelookDBUser]
+							ALTER ROLE [db_ddladmin] ADD MEMBER [OnelookDBUser]
+							GRANT EXECUTE TO  [OnelookDBUser] ;
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N''''XSUNT\OneLook_SQL_CN_Admins'''')
+	DROP USER [XSUNT\OneLook_SQL_CN_Admins]
+CREATE USER [XSUNT\OneLook_SQL_CN_Admins]FOR LOGIN [XSUNT\OneLook_SQL_CN_Admins]
+ALTER USER [XSUNT\OneLook_SQL_CN_Admins] WITH DEFAULT_SCHEMA=[dbo]
+ALTER ROLE [db_datareader] ADD MEMBER [XSUNT\OneLook_SQL_CN_Admins]
+ALTER ROLE [db_datawriter] ADD MEMBER [XSUNT\OneLook_SQL_CN_Admins]
+ALTER ROLE [db_ddladmin] ADD MEMBER [XSUNT\OneLook_SQL_CN_Admins]
+GRANT EXECUTE TO  [XSUNT\OneLook_SQL_CN_Admins];
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N''''XSUNT\OneLook_CN_SQL_RW'''')
+	DROP USER [XSUNT\OneLook_CN_SQL_RW]
+CREATE USER [XSUNT\OneLook_CN_SQL_RW]FOR LOGIN [XSUNT\OneLook_CN_SQL_RW]
+ALTER USER [XSUNT\OneLook_CN_SQL_RW] WITH DEFAULT_SCHEMA=[dbo]
+ALTER ROLE [db_datareader] ADD MEMBER [XSUNT\OneLook_CN_SQL_RW]
+ALTER ROLE [db_datawriter] ADD MEMBER [XSUNT\OneLook_CN_SQL_RW]
+ALTER ROLE [db_ddladmin] ADD MEMBER [XSUNT\OneLook_CN_SQL_RW]
+GRANT EXECUTE TO  [XSUNT\OneLook_CN_SQL_RW] ;       
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N''''XSUNT\OneLook_CN_SQL_RO'''')
+	DROP USER [XSUNT\OneLook_CN_SQL_RO]
+CREATE USER [XSUNT\OneLook_CN_SQL_RO]FOR LOGIN [XSUNT\OneLook_CN_SQL_RO]
+ALTER USER [XSUNT\OneLook_CN_SQL_RO] WITH DEFAULT_SCHEMA=[dbo]
+ALTER ROLE [db_datareader] ADD MEMBER [XSUNT\OneLook_CN_SQL_RO]
+ 
+ 
+			''
+		 	print @V_SQL 
+			EXEC  master.dbo.sp_executesql @V_SQL
+ FETCH NEXT
+FROM
+      Db_cursor
+INTO
+      @dbname
+
+End
+
+CLOSE Db_cursor
+DEALLOCATE Db_cursor
+
+DECLARE @html nvarchar(MAX), @body1 Nvarchar(max) 
+EXEC [master].[dbo].[spQueryToHtmlTable]  @html = @html OUTPUT,  @query = N''SELECT
+       distinct Name,cast(create_date as date) as Create_Date
+FROM
+            [Master].sys.databases
+ where create_date >= cast(getdate()-1 as date)
+ and name not in (
+''''master''''
+,''''model''''
+,''''msdb''''
+,''''tempdb'''')
+and name not like ''''%BMS%CHINA%''''
+and name not like ''''%XsuntAdmin%''''
+and name not like ''''%BMSHEME%''''
+and not  (name like ''''%BMSONC%''''
+or name like ''''%onelook%''''
+or name like ''''%HIT%''''
+or name like ''''%account%''''
+)
+'' ,@orderBy = N''ORDER BY 1'';
+print @html
+
+set @body1 = case when @html is null then ''NO Databases asssigned OnelookDBUser DB Roles for CV , IMM , VAP ,ZEP <br> Regards </br>  OneLookOncology Team''
+ else ''List of Databases OnelookDBUser DB Role Assigned on PA103 for CV , IMM , VAP ,ZEP  <br> <br> ''+@html + ''<br> Regards </br>  OneLookOncology Team'' end 
+ 
+
+
+EXEC master.dbo. sys_sp_send_dbmail 
+    @profile_name = ''XSUNT ONELOOK-DB-DEV NOTIFICATION'',
+    @recipients = ''prateek.singh@xsunt.com;alen.zhang@xsunt.com;akorolev@xsunt.com;hannah.zheng@xsunt.com;feng.lu@xsunt.com'',
+    @subject = ''List of Databases OnelookDBUser DB Role Assigned on PA103 for CV , IMM , VAP ,ZEP'',
+    @body = @body1 ,
+    @body_format = ''HTML'',
+    @query_no_truncate = 1,
+    @attach_query_result_as_file = 0;
+	 
+', 
+		@database_name=N'master', 
+		@output_file_name=N'W:\work\Process\Daily\Logs\1APS_DB Role Assigment_OneLookDbUSer.txt', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Runt to assign dbroles', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=8, 
+		@freq_subday_interval=10, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20250513, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959, 
+		@schedule_uid=N'40c0f230-dba5-4d6d-9406-01464b8de769'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+GO
+
--- 172.25.99.104/Sys Backup Full.sql
+++ 172.25.99.104/Sys Backup Full.sql
@@ -9,7 +9,7 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 END
 
 DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'AI Data Export Weekly Processing', 
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Sys_Backup_Full', 
 		@enabled=1, 
 		@notify_level_eventlog=0, 
 		@notify_level_email=0, 
@@ -18,51 +18,50 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'AI Data Export Weekly Proces
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'[Uncategorized (Local)]', 
-		@owner_login_name=N'XSUNT\yancheng.zhou', @job_id = @jobId OUTPUT
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Run the sp', 
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Sys_Backup_Full', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
 		@on_success_step_id=0, 
 		@on_fail_action=2, 
 		@on_fail_step_id=0, 
-		@retry_attempts=0, 
-		@retry_interval=0, 
+		@retry_attempts=3, 
+		@retry_interval=5, 
 		@os_run_priority=0, @subsystem=N'TSQL', 
-		@command=N'exec [XSUNT_AI_DataExport_Test].[dbo].[sp_utility_Weekly_AIDE_EXEC]', 
-		@database_name=N'master', 
+		@command=N'delete from [XsuntAdmin].[dbo].[sys_commandlog] where [EndTime] < getdate()-40;
+GO
+exec [XsuntAdmin].[dbo].[sys_database_backup]
+@Databases = ''ALL_DATABASES'',
+@Directory = ''\\172.25.99.17\pa-bms-bck\ONELOOK-DB-DEV1'',
+@BackupType = ''FULL'',
+@Compress = ''Y'',
+@DatabasesInParallel = ''Y'',
+@NumberOfFiles = 10,
+@MinBackupSizeForMultipleFiles = 100000,
+@BlockSize = 65536,
+@MaxTransferSize = 4194304,
+@LogToTable = ''Y'',
+@Execute = ''Y''', 
+		@database_name=N'XsuntAdmin', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'AIDE Tuesdata Processing', 
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Sys_Backup_Full', 
 		@enabled=1, 
 		@freq_type=8, 
-		@freq_interval=4, 
+		@freq_interval=1, 
 		@freq_subday_type=1, 
 		@freq_subday_interval=0, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=1, 
-		@active_start_date=20250610, 
+		@active_start_date=20180528, 
 		@active_end_date=99991231, 
-		@active_start_time=220000, 
+		@active_start_time=0, 
 		@active_end_time=235959, 
-		@schedule_uid=N'cb078242-b74f-4871-8e90-8ac72caace0d'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'sp_utility_Weekly_AIDE_EXEC', 
-		@enabled=1, 
-		@freq_type=8, 
-		@freq_interval=2, 
-		@freq_subday_type=1, 
-		@freq_subday_interval=0, 
-		@freq_relative_interval=0, 
-		@freq_recurrence_factor=1, 
-		@active_start_date=20250604, 
-		@active_end_date=99991231, 
-		@active_start_time=220000, 
-		@active_end_time=235959, 
-		@schedule_uid=N'bd8cc216-bcb5-45ed-9ff7-9e75234f6e2f'
+		@schedule_uid=N'1e8c2917-0634-49d4-aa28-3a5a532bcf92'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
