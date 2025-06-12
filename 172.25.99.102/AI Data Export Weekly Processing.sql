8ac4543683002e32fab90a57ed3883d42097a95b
svc-xsunt-jenkins <svc-jenkins@xsunt.com>
2025-06-11 22:25:31 -0400

    Push latest 2025-06-11 10:25:31

--- 172.25.99.102/AI Data Export Weekly Processing.sql
+++ 172.25.99.102/AI Data Export Weekly Processing.sql
@@ -39,7 +39,7 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'AIDE Tuesdata Processing', 
 		@enabled=1, 
 		@freq_type=8, 
-		@freq_interval=4, 
+		@freq_interval=12, 
 		@freq_subday_type=1, 
 		@freq_subday_interval=0, 
 		@freq_relative_interval=0, 
