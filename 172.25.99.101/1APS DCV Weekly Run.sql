16215b7599426c92729019e25d064e3d1959f54c
svc-xsunt-jenkins <svc-jenkins@xsunt.com>
2025-08-18 22:25:32 -0400

    Push latest 2025-08-18 10:25:32

--- 172.25.99.101/1APS DCV Weekly Run.sql
+++ 172.25.99.101/1APS DCV Weekly Run.sql
@@ -56,20 +56,6 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'run weekl
 		@active_end_time=235959, 
 		@schedule_uid=N'4f7d687a-17ad-4636-a76d-3dc454cef347'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'weekly DCV Schedule', 
-		@enabled=1, 
-		@freq_type=8, 
-		@freq_interval=1, 
-		@freq_subday_type=1, 
-		@freq_subday_interval=0, 
-		@freq_relative_interval=0, 
-		@freq_recurrence_factor=1, 
-		@active_start_date=20220707, 
-		@active_end_date=99991231, 
-		@active_start_time=180000, 
-		@active_end_time=235959, 
-		@schedule_uid=N'1958f9e3-a585-445e-8200-91aaeb8ebdaf'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 COMMIT TRANSACTION
